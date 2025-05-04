79a78f655ba2a5d649c81e4b37173cf7c0eccabf
santos0210 <adeilton210@gmail.com>
2025-05-04 11:16:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var setaDireita = window.document.getElementById["seta-direita"]
+var cardBgroud = window.document.getElementById["cardBgroud"]
+var cardBruna = window.document.getElementById["cardBruna"]
+var cardOrcamento = window.document.getElementById["cardOrcamento"]
+var setaEsquerda = window.document.getElementById["seta-esquerda"]
+
+function RolarParaDireita{} [
+    cardOrcamento.style = "diplay: none"
+    cardBgroud.style = "diplay: flex"
+    setaDireita.style = "display: flex"
+    setaEsquerda.style = "display: none"
+]
+function RolarParaEsquerda() {
+    cardOrcamento.style ="display: flex"
+    cardBgroud.style ="display: none"
+    setaDireita.style ="diplay:none"
+    setaEsquerda.style ="display:flex"
+}
+
+
+    
+
+
+
+    
+
+    
+
